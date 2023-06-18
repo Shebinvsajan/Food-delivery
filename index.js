af9f1c2233721d85f03abcbe12626b5689522ce9
shebin v sajan <shebinvsajan@gmail.com>
2023-06-18 10:19:13 +0530

    cors updated

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 const express = require('express');
-const db =require("./db.js")
-const menu = require("./Models/menuModel.js");
+const cors = require('cors');
+const db = require('./db.js');
+const menu = require('./Models/menuModel.js');
 const app = express();
 const port = 8000;
-const menuRoute = require('./routes/menuRoute.js')
-const userRoute = require('./routes/userRoute.js')
-const loginRoute = require('./routes/loginRoute.js')
-// const placeorder = require('./routes/oderRoute.js')
-
+const menuRoute = require('./routes/menuRoute.js');
+const userRoute = require('./routes/userRoute.js');
+const loginRoute = require('./routes/loginRoute.js');
 
 // Middleware
+app.use(cors({origin: '*'}))
 app.use(express.json());
 
 // Routes
-app.get('/', (req,  res) => {
+app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// get api in mongdb
-
-app.use('/api/menu/',menuRoute)
-
-// add new user
-app.use('/api/users/',userRoute)
-// Login
-app.use('/api/user/',loginRoute)
-
-// place oder
-
-// app.use('/api/order/',placeorder)
-
-
+app.use('/api/menu/', menuRoute);
+app.use('/api/users/', userRoute);
+app.use('/api/user/', loginRoute);
 
 // Start the server
 app.listen(port, () => {
